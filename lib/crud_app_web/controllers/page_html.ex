0973c23528b67a6181009584895ac7400f80a8b4
Your Name <you@example.com>
2026-05-06 21:39:32 +0530

    Add page_html.ex

---
+++ lib/crud_app_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule CrudAppWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use CrudAppWeb, :html
+
+  embed_templates "page_html/*"
+end
