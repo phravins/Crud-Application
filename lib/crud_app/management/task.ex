520778e4c9202adddfa609311d88cf31bc78e8bf
Your Name <you@example.com>
2026-05-06 21:39:32 +0530

    Add task.ex

---
+++ lib/crud_app/management/task.ex
@@ -0,0 +1,19 @@
+defmodule CrudApp.Management.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tasks" do
+    field :title, :string
+    field :description, :string
+    field :status, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(task, attrs) do
+    task
+    |> cast(attrs, [:title, :description, :status])
+    |> validate_required([:title, :description, :status])
+  end
+end
