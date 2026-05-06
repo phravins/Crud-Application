4db7026a9cb8422ba0efd92e6136dc2687ef1796
Your Name <you@example.com>
2026-05-06 21:39:32 +0530

    Add index.ex

---
+++ lib/crud_app_web/live/task_live/index.ex
@@ -0,0 +1,65 @@
+defmodule CrudAppWeb.TaskLive.Index do
+  use CrudAppWeb, :live_view
+
+  alias CrudApp.Management
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash}>
+      <.header>
+        Listing Tasks
+        <:actions>
+          <.button variant="primary" navigate={~p"/tasks/new"}>
+            <.icon name="hero-plus" /> New Task
+          </.button>
+        </:actions>
+      </.header>
+
+      <.table
+        id="tasks"
+        rows={@streams.tasks}
+        row_click={fn {_id, task} -> JS.navigate(~p"/tasks/#{task}") end}
+      >
+        <:col :let={{_id, task}} label="Title">{task.title}</:col>
+        <:col :let={{_id, task}} label="Description">{task.description}</:col>
+        <:col :let={{_id, task}} label="Status">{task.status}</:col>
+        <:action :let={{_id, task}}>
+          <div class="sr-only">
+            <.link navigate={~p"/tasks/#{task}"}>Show</.link>
+          </div>
+          <.link navigate={~p"/tasks/#{task}/edit"}>Edit</.link>
+        </:action>
+        <:action :let={{id, task}}>
+          <.link
+            phx-click={JS.push("delete", value: %{id: task.id}) |> hide("##{id}")}
+            data-confirm="Are you sure?"
+          >
+            Delete
+          </.link>
+        </:action>
+      </.table>
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:page_title, "Listing Tasks")
+     |> stream(:tasks, list_tasks())}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    task = Management.get_task!(id)
+    {:ok, _} = Management.delete_task(task)
+
+    {:noreply, stream_delete(socket, :tasks, task)}
+  end
+
+  defp list_tasks() do
+    Management.list_tasks()
+  end
+end
