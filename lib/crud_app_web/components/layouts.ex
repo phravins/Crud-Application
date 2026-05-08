60de74dcabd2ed64c4c7afe881c334752ad78acb
Your Name <you@example.com>
2026-05-08 15:14:27 +0530

    Add/Update lib/crud_app_web/components/layouts.ex

--- lib/crud_app_web/components/layouts.ex
+++ lib/crud_app_web/components/layouts.ex
@@ -61,8 +61,8 @@ defmodule CrudAppWeb.Layouts do
       </div>
     </header>
 
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
+    <main class="px-4 py-12 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-7xl space-y-4">
         {render_slot(@inner_block)}
       </div>
     </main>
