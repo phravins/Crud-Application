6efb9c37a7c06a2c23fde2a94c9f41862c8ce969
Your Name <you@example.com>
2026-05-08 15:14:27 +0530

    Add/Update lib/crud_app/management.ex

--- lib/crud_app/management.ex
+++ lib/crud_app/management.ex
@@ -8,47 +8,39 @@ defmodule CrudApp.Management do
 
   alias CrudApp.Management.Task
 
+  alias CrudApp.Accounts.Scope
+
   @doc """
   Returns the list of tasks.
-
-  ## Examples
-
-      iex> list_tasks()
-      [%Task{}, ...]
-
   """
+  def list_tasks(%Scope{} = scope) do
+    Repo.all_by(Task, assigned_to: scope.user.id)
+  end
+
   def list_tasks do
     Repo.all(Task)
   end
 
   @doc """
   Gets a single task.
-
-  Raises `Ecto.NoResultsError` if the Task does not exist.
-
-  ## Examples
-
-      iex> get_task!(123)
-      %Task{}
-
-      iex> get_task!(456)
-      ** (Ecto.NoResultsError)
-
   """
+  def get_task!(%Scope{} = scope, id) do
+    Repo.get_by!(Task, [id: id, assigned_to: scope.user.id])
+  end
+
   def get_task!(id), do: Repo.get!(Task, id)
 
   @doc """
   Creates a task.
-
-  ## Examples
-
-      iex> create_task(%{field: value})
-      {:ok, %Task{}}
-
-      iex> create_task(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
+  def create_task(%Scope{} = scope, attrs) do
+    %Task{}
+    |> Task.changeset(attrs)
+    |> Ecto.Changeset.put_change(:assigned_to, scope.user.id)
+    |> Ecto.Changeset.put_change(:user_email, scope.user.email)
+    |> Repo.insert()
+  end
+
   def create_task(attrs) do
     %Task{}
     |> Task.changeset(attrs)
@@ -57,16 +49,16 @@ defmodule CrudApp.Management do
 
   @doc """
   Updates a task.
-
-  ## Examples
-
-      iex> update_task(task, %{field: new_value})
-      {:ok, %Task{}}
-
-      iex> update_task(task, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
+  def update_task(%Scope{} = scope, %Task{} = task, attrs) do
+    true = task.assigned_to == scope.user.id
+    task
+    |> Task.changeset(attrs)
+    |> Ecto.Changeset.put_change(:assigned_to, scope.user.id)
+    |> Ecto.Changeset.put_change(:user_email, scope.user.email)
+    |> Repo.update()
+  end
+
   def update_task(%Task{} = task, attrs) do
     task
     |> Task.changeset(attrs)
@@ -75,30 +67,33 @@ defmodule CrudApp.Management do
 
   @doc """
   Deletes a task.
-
-  ## Examples
-
-      iex> delete_task(task)
-      {:ok, %Task{}}
-
-      iex> delete_task(task)
-      {:error, %Ecto.Changeset{}}
-
   """
+  def delete_task(%Scope{} = scope, %Task{} = task) do
+    true = task.assigned_to == scope.user.id
+    Repo.delete(task)
+  end
+
   def delete_task(%Task{} = task) do
     Repo.delete(task)
   end
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking task changes.
-
-  ## Examples
-
-      iex> change_task(task)
-      %Ecto.Changeset{data: %Task{}}
-
   """
-  def change_task(%Task{} = task, attrs \\ %{}) do
+  def change_task(scope_or_task, task_or_attrs, attrs \\ %{})
+
+  def change_task(%Scope{} = scope, %Task{} = task, attrs) do
+    if task.assigned_to do
+      true = task.assigned_to == scope.user.id
+    end
+
+    task
+    |> Task.changeset(attrs)
+    |> Ecto.Changeset.put_change(:assigned_to, scope.user.id)
+    |> Ecto.Changeset.put_change(:user_email, scope.user.email)
+  end
+
+  def change_task(%Task{} = task, attrs, _unused) do
     Task.changeset(task, attrs)
   end
 end
